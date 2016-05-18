f1c501dd3724ba0ca6704166d947982386658a59
zepsen <zepsen@ZEPSEN-PC>
2016-05-18 18:00:22 +0300

    add nsubstitute

---
+++ NSub/ReturningValuesWithNSub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NSubstitute;
+using xUnitTest;
+using Xunit;
+
+namespace NSub
+{
+    
+    public class ReturningValuesWithNSub
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var fake = Substitute.For<IExtensionManager>();
+
+            fake.IsValidFile().Returns(true);
+
+            Assert.True(fake.IsValidFile());
+        }
+    }
+}
---
+++ NSub/TrowingFakeExeptionWithNSubs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NSubstitute;
+using xUnitTest;
+using xUnitTest.Interfaces;
+using Xunit;
+
+namespace NSub
+{
+    public class TrowingFakeExeptionWithNSubs
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var fake = Substitute.For<IWebService>();
+            fake.When(i => i.LogError(Arg.Any<string>()))
+                .Do(context =>
+                {
+                    throw new Exception("Fake exeption");
+                });
+
+            Assert.Throws<Exception>(() => fake.LogError("str"));
+        }
+    }
+}
