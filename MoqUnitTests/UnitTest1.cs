455a4adcab7494276766e6c2b2b3eb54fdbff8a0
zepsen <zepsen@ZEPSEN-PC>
2016-05-19 17:58:48 +0300

    more moq tests

---
+++ MoqUnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Moq;
+using xUnitTest.Events;
+using xUnitTest.Interfaces;
+using Xunit;
+
+namespace MoqUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var stub = new Mock<IViewEvent>();
+            var testClass = new TestClassEvents(stub.Object);
+
+            stub.Raise(x => x.Loaded += null);
+            stub.Verify(x => x.Render("Test"));
+            
+
+        }
+    }
+}
